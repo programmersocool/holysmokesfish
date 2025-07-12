39263cf9daf5789d274a8944fa5d97563a334fdd
programmersocool <evanthecoolkid888@gmail.com>
2025-07-11 19:23:00 -0700

    Update ballsack.lua

--- ballsack.lua
+++ ballsack.lua
@@ -314,7 +314,6 @@ do
 			Adornee = adorneePart,
 			Text = model.Name,
 			TextColor = itemConfig.Color
-			StudsOffset = Vector3.new(0, 0, 0)
 		})
 
 		local connection = model.AncestryChanged:Connect(function(_, parent)
